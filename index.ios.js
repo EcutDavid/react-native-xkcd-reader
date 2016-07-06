6b45d7ce2904481d18f55e86b1def0480e0c0271
David Guan <davidguandev@gmail.com>
2016-07-06 21:59:20 +0800

    Render comics.

--- index.ios.js
+++ index.ios.js
@@ -4,22 +4,75 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
+  Dimensions,
   Image,
   StyleSheet,
   Text,
   View,
   ScrollView
-} from 'react-native';
+} from 'react-native'
+import request from 'superagent'
 
 const arr = [1,2,32,3,234,23,423,4,234]
+const API_ROOT = 'http://139.59.6.112:9021/'
+
 class reactNativeXkcdReader extends Component {
-  componentDidMount() {
+  constructor() {
+    super()
+    this.state = {
+      comics: [],
+      comicIndex: 0,
+      loading: false,
+      searchResult: []
+    }
+  }
+
+  fetchComics(index, incComicIndex) {
+    if (this.inFetching) {
+      return
+    }
+    this.inFetching = true
+    request
+      .get(`${API_ROOT}${index}`)
+      .end((err, res) => {
+        this.inFetching = false
+        if (!err) {
+          const { comics } = this.state
+          const newComics = JSON.parse(res.text)
+          if (comics.every(d => d.title != newComics[0].title)) {
+            this.setState({ comics: comics.concat(newComics) })
+          }
+          if (incComicIndex) {
+            this.setState({ comicIndex: index })
+          }
+        } else {
+          console.error(err)
+        }
+      })
+  }
+
+  componentWillMount() {
+    this.fetchComics(0)
+  }
+
+  onScrollHandler(e) {
+    const {
+      layoutMeasurement : { height: layoutHeight },
+      contentSize : { height: contentHeight  },
+      contentOffset : { y: scrollTop }
+    } = e.nativeEvent
+
+    if (contentHeight - scrollTop - layoutHeight < 200) {
+      const { comicIndex } = this.state
+      this.fetchComics(comicIndex + 1, true)
+    }
   }
 
   render() {
+    const { comics, loading, searchResult } = this.state
     return (
       <View style={styles.container}>
         <View style={styles.header}>
@@ -29,26 +82,38 @@ class reactNativeXkcdReader extends Component {
           </Text>
           <View style={{ flex: 1 }} />
         </View>
-        <ScrollView>
+        <ScrollView
+          style={styles.content}
+          onScroll={e => this.onScrollHandler(e)}
+          scrollEventThrottle={200}
+        >
+          <View
+            style= {{ alignItems: 'center' }}
+          >
           {
-            arr.map((d, key) => (
-              <View key={key}>
-                <Image
-                  style={{ height: 50, width: 50 }}
-                  source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-                />
-                <Text>
-                  123123
+            comics.map((d, key) => (
+              <View key={key} style= {{ alignItems: 'center' }}>
+                <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+                  {d.title}
                 </Text>
+                <View style={{ flexDirection: 'row', height: 400 }}>
+                  <Image
+                    resizeMode='contain'
+                    source={{ uri: d.img }}
+                    style={{ flex: 1, height: undefined, marginLeft: 10, marginRight: 10 }}
+                  />
+                </View>
               </View>
             ))
           }
+          </View>
         </ScrollView>
       </View>
-    );
+    )
   }
 }
 
+const width = Dimensions.get('window').width
 const styles = StyleSheet.create({
   header: {
     marginTop: 20,
@@ -62,7 +127,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginTop: 5,
     marginBottom: 5,
-    fontSize: 32,
+    fontSize: 36,
   },
   container: {
     flex: 1,
@@ -70,16 +135,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  content: {
+    marginTop: 20,
+    width: width
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  comicLine: {
   },
-});
+})
 
-AppRegistry.registerComponent('reactNativeXkcdReader', () => reactNativeXkcdReader);
+AppRegistry.registerComponent('reactNativeXkcdReader', () => reactNativeXkcdReader)
