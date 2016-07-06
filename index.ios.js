a2eb12116979ea4f662193ad5ebecc0b89829cad
David Guan <davidguandev@gmail.com>
2016-07-06 20:51:50 +0800

    Implement header.

--- index.ios.js
+++ index.ios.js
@@ -7,31 +7,63 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView
 } from 'react-native';
 
+const arr = [1,2,32,3,234,23,423,4,234]
 class reactNativeXkcdReader extends Component {
+  componentDidMount() {
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <View style={styles.header}>
+          <View style={{ flex: 1 }} />
+          <Text style={styles.title}>
+            xkcdReader
+          </Text>
+          <View style={{ flex: 1 }} />
+        </View>
+        <ScrollView>
+          {
+            arr.map((d, key) => (
+              <View key={key}>
+                <Image
+                  style={{ height: 50, width: 50 }}
+                  source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+                />
+                <Text>
+                  123123
+                </Text>
+              </View>
+            ))
+          }
+        </ScrollView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  header: {
+    marginTop: 20,
+    backgroundColor: 'skyblue',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    marginTop: 5,
+    marginBottom: 5,
+    fontSize: 32,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
